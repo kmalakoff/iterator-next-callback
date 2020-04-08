a5b25dac0478df3ef0ab9ee41af7549e34702235
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-07 19:20:23 -0700

    Add tests for build in iterators

--- compatibility/spec/asyncIterator.js
+++ compatibility/spec/asyncIterator.js
@@ -37,4 +37,45 @@ describe('asyncIterator', function () {
         assert.ok(!err);
       });
   });
+
+  it('it should add a callback interface with synchronous built-ins', function (done) {
+    var iterable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var iterator = iterable[Symbol.iterator]();
+    var callback = nextCallback(iterator);
+
+    var result = iterator.next();
+    assert.equal(result.value, 1);
+
+    callback(function (err1, value1) {
+      assert.ok(!err1);
+      assert.equal(value1, 2);
+      done();
+    });
+  });
+
+  it('it should add a callback interface with asynchronous built-ins', function (done) {
+    async function* createAsyncIterable(iterable) {
+      for (const elem of iterable) {
+        yield elem;
+      }
+    }
+
+    var iterator = createAsyncIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    var callback = nextCallback(iterator);
+
+    iterator
+      .next()
+      .then(function (result) {
+        assert.equal(result.value, 1);
+
+        callback(function (err1, value1) {
+          assert.ok(!err1);
+          assert.equal(value1, 2);
+          done();
+        });
+      })
+      .catch(function (err) {
+        assert.ok(!err);
+      });
+  });
 });
