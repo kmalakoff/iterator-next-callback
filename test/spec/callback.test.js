84ef9235df8aba8d7722f5bb869f754c5c01ab31
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 21:27:07 -0700

    Lint

--- test/spec/callback.test.js
+++ test/spec/callback.test.js
@@ -12,13 +12,13 @@ Iterator.prototype.next = function (callback) {
 describe('callback', function () {
   it('it should add a callback interface', function (done) {
     var iterator = new Iterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    var callback = nextCallback(iterator);
+    var iteratorCallback = nextCallback(iterator);
 
     iterator.next(function (err, value) {
       assert.ok(!err);
       assert.equal(value, 1);
 
-      callback(function (err1, value1) {
+      iteratorCallback(function (err1, value1) {
         assert.ok(!err1);
         assert.equal(value1, 2);
         done();
