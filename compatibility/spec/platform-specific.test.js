12d7ab35465dd875d8aec26ad85304fea55ee8e1
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-26 07:07:33 -0700

    Update compatibility

--- compatibility/spec/platform-specific.test.js
+++
@@ -1,5 +0,0 @@
-const HAS_ASYNC_ITERATOR = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
-
-describe('platform specific', function () {
-  if (HAS_ASYNC_ITERATOR) require('./asyncIterator');
-});
