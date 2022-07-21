12e02696a93bab108503ba403c14ff589f78b3e6
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-20 20:55:59 -0700

    Restructure library

--- test/spec/platform-specific.test.js
+++ test/spec/platform-specific.test.js
@@ -1,5 +1,3 @@
 const HAS_ASYNC_ITERATOR = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 
-describe('platform specific', function () {
-  if (HAS_ASYNC_ITERATOR) require('./asyncIterator');
-});
+!HAS_ASYNC_ITERATOR || require('./asyncIterator');
