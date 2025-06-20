2b7c6753ae4e39d0571ff13844dba014bb6ae41a
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:21:34 -0700

    Upgrade modules

--- test/unit/asyncIterator.test.ts
+++ test/unit/asyncIterator.test.ts
@@ -19,14 +19,13 @@ class Iterator<T> implements AsyncIterator<T> {
 describe('promise', () => {
   (() => {
     // patch and restore promise
-    // @ts-ignore
-    let rootPromise: Promise;
+    if (typeof global === 'undefined') return;
+    const globalPromise = global.Promise;
     before(() => {
-      rootPromise = global.Promise;
       global.Promise = Pinkie;
     });
     after(() => {
-      global.Promise = rootPromise;
+      global.Promise = globalPromise;
     });
   })();
 
