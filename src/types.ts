a7d1f3f8454a23af43c7b9b9d64871b236061334
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 16:31:36 -0800

    Migrate to void

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,2 @@
 export type Callback<T, TReturn = unknown> = (error?: Error, result?: IteratorResult<T, TReturn>) => void;
-export type IteratorCallback<T> = (callback?: Callback<T>) => undefined;
+export type IteratorCallback<T> = (callback?: Callback<T>) => void;
