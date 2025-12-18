a7bfbffca9fba0c850ce59979f58d9a1a191501d
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 16:31:36 -0800

    Migrate to void

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from './types.ts';
 
 export default function iteratorNextCallback<T, TReturn = unknown, TNext = unknown>(iterator: AsyncIterator<T, TReturn, TNext> | AsyncIterable<T, TReturn, TNext> | AsyncIterableIterator<T, TReturn, TNext>): IteratorCallback<T> {
   if (typeof Symbol !== 'undefined' && Symbol.asyncIterator && iterator[Symbol.asyncIterator]) {
-    return function nextAsyncIterable(callback: Callback<T>): undefined {
+    return function nextAsyncIterable(callback: Callback<T>): void {
       iterator[Symbol.asyncIterator]()
         .next()
         .then((result) => {
@@ -17,7 +17,7 @@ export default function iteratorNextCallback<T, TReturn = unknown, TNext = unkno
     };
   }
 
-  return function AsyncIterator(callback: Callback<T>): undefined {
+  return function AsyncIterator(callback: Callback<T>): void {
     const result = (iterator as AsyncIterator<T, TReturn>).next();
     if (isPromise(result)) {
       result
